13cce5f1f8122ae882da792119b636048b961e20
DJTheCreator <diegocajiao@outlook.com>
2024-04-01 00:04:53 -0400

    -Fixed 'sticky' border on Project.html

--- js/app.js
+++ js/app.js
@@ -1,60 +1,54 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// gsap.to("#event1",{
-//   opacity: 1,
-//   x:200,
-//   duration: 4,
-//   scrollTrigger: {
-//     trigger: "#event1",
-//     start: "center 90%",
-//     end: "center 60%",
-//     scrub: true,
-//     toggleActions: "restart none none none",
-//     markers: {startColor: "blue", endColor: "orange"},
-//   }
-// })
-
 const items = gsap.utils.toArray('.item-text');
 items.forEach((item)=>{
   gsap.fromTo(item,{
     opacity: 1,
+    scale:2,
     duration: 4,
     scrollTrigger: {
       trigger: item,
-      start: "center 30%",
-      end: "center 5%",
+      start: "center 50%",
+      end: "center 10%",
       scrub: true,
+      // markers:true,
       toggleActions: "restart none none none",
     }
   }, {
     opacity: 0,
+    scale:1,
     duration: 4,
     scrollTrigger: {
       trigger: item,
-      start: "center 30%",
-      end: "center 5%",
+      start: "center 50%",
+      end: "center 10%",
       scrub: true,
+      // markers:true,
       toggleActions: "restart none none none",
     }
   });
   gsap.fromTo(item,{
     opacity: 0,
+    scale:1,
     duration: 4,
     scrollTrigger: {
       trigger: item,
       start: "center 100%",
-      end: "center 40%",
+      end: "center 60%",
       scrub: true,
+      // markers: {endColor: "blue", startColor: "orange"},
       toggleActions: "restart none none none",
     }
   }, {
     opacity: 1,
+    scale:2,
     duration: 4,
     scrollTrigger: {
       trigger: item,
       start: "center 100%",
-      end: "center 40%",
+      end: "center 60%",
       scrub: true,
+      // markers: {endColor: "blue", startColor: "orange"},
       toggleActions: "restart none none none",
     }
   });
