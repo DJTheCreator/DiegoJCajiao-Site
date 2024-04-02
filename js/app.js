f598495820291e47416ee680bb636ed2a6b226d2
DJTheCreator <diegocajiao@outlook.com>
2024-04-01 21:02:46 -0400

    -Created center border with sticky

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 gsap.registerPlugin(ScrollTrigger);
 
+window.onbeforeunload = function () {
+  window.scrollTo(0,0);
+};
+
 const items = gsap.utils.toArray('.item-text');
 items.forEach((item)=>{
   gsap.fromTo(item,{
@@ -52,4 +56,30 @@ items.forEach((item)=>{
       toggleActions: "restart none none none",
     }
   });
+})
+
+let borderClasses = document.getElementById("ProjectTimelineBorder").classList;
+ScrollTrigger.create({
+  trigger: document.getElementById("ProjectTimelineBorder"),
+  start: "center 50%",
+  end: "center 50%",
+  // markers: true,
+  onEnter: () => document.getElementById("ProjectTimelineBorder").classList.add('enableFixed'),
+  onEnterBack: () => document.getElementById("ProjectTimelineBorder").classList.remove('enableFixed')
+})
+
+let fakeBorderClasses = document.getElementById("fakeEndBorder").classList;
+ScrollTrigger.create({
+  trigger: document.getElementById("ProjectTimeline"),
+  start: "bottom 95%",
+  end: "bottom 95%",
+  markers: true,
+  onEnter: () => {
+    borderClasses.remove('enableFixed');
+    borderClasses.add('timelineBottom');
+    fakeBorderClasses.add('timelineBottom')},
+  onEnterBack: () => {
+    borderClasses.add('enableFixed');
+    borderClasses.remove('timelineBottom');
+    fakeBorderClasses.remove('timelineBottom')},
 })
